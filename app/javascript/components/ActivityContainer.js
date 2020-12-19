9417890aa1b53a6ef556d886430098cb77df1742
Sebastian Almeida <18198367+sebasalmeidam@users.noreply.github.com>
2020-12-19 17:12:06 -0500

    react installed

---
+++ app/javascript/components/ActivityContainer.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class ActivityContainer extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <div>hola</div>    
+      </React.Fragment>
+    );
+  }
+}
+
+export default ActivityContainer
