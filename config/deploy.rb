8c2c14aefd50715de938a965d72f96c2a7f7767a
Sebastian Almeida <18198367+sebasalmeidam@users.noreply.github.com>
2020-12-21 10:25:11 -0500

    capistrano initial configs

---
+++ config/deploy.rb
@@ -0,0 +1,73 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.14.1"
+
+set :application, "plog"
+set :repo_url, "git@github.com:sebasalmeidam/plog.git"
+
+
+set :migration_role, [:app]
+set :keep_releases, 3
+set :pty,  false
+set :rvm_ruby_version, '2.6.6'
+
+set :migration_role, [:app]
+set :keep_releases, 3
+set :pty,  false
+set :rvm_ruby_version, '2.3.3'
+
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/initializers/prod_environment.rb', 'config/master.key')
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/system public/uploads}
+
+namespace :deploy do
+  task :restart do
+    on roles(:app), in: :sequence do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, :restart
+  after 'deploy', 'deploy:migrate'
+end
+
+namespace :logs do
+  desc "tail rails logs" 
+  task :tail_rails do
+    on roles(:app) do
+      execute "tail -f #{shared_path}/log/production.log"
+    end
+  end
+end
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
